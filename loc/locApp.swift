04ac6d918d5194e52eb164f3ce0d6e7899960bdd
drewhartsfield <drewharts8@gmail.com>
2024-07-13 11:50:20 -0600

    Initial Commit

---
+++ loc/locApp.swift
@@ -0,0 +1,17 @@
+//
+//  locApp.swift
+//  loc
+//
+//  Created by Andrew Hartsfield II on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct locApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
