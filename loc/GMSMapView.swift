77af86202b0f11e7aa23872f1890998dc8454061
drewhartsfield <drewharts8@gmail.com>
2024-11-03 12:02:12 -0700

    First commit, working current location google maps

---
+++ loc/GMSMapView.swift
@@ -0,0 +1,8 @@
+//
+//  GMSMapView.swift
+//  loc
+//
+//  Created by Andrew Hartsfield II on 7/13/24.
+//
+
+import Foundation
---
+++ loc/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  loc
+//
+//  Created by Andrew Hartsfield II on 8/7/24.
+//
+
+import Foundation
