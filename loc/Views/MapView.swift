071cf367b554469359c2ab2cf1157503d3aaad0b
drewhartsfield <drewharts8@gmail.com>
2024-11-06 15:48:50 -0700

    fixed issue where map would reset back to current location after scrolling around. Inadvertantly when searching places, the locations are more specific to current location too.

--- loc/Views/MapView.swift
+++ loc/Views/MapView.swift
@@ -12,33 +12,33 @@ import GooglePlaces
 struct MapView: UIViewRepresentable {
     @Binding var searchResults: [GMSAutocompletePrediction]
     @Binding var selectedPlace: GMSPlace?
-    @ObservedObject var locationManager: LocationManager // Observe the location manager
+    @ObservedObject var locationManager: LocationManager
+    @State private var hasCenteredOnUser = false
 
     let mapView = GMSMapView()
 
     func makeUIView(context: Context) -> GMSMapView {
         mapView.isMyLocationEnabled = true
 
-        // Set the camera to the current location when the view is created
-        if let currentLocation = locationManager.currentLocation {
-            let camera = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude,
-                                                  longitude: currentLocation.coordinate.longitude,
-                                                  zoom: 15.0)
-            mapView.camera = camera
+        // Set a default camera position first (e.g., a global view)
+        let defaultCamera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: 1.0)
+        mapView.camera = defaultCamera
+
+        // Delay briefly, then animate to user's current location if available
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            if let currentLocation = locationManager.currentLocation, !hasCenteredOnUser {
+                let camera = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude,
+                                                      longitude: currentLocation.coordinate.longitude,
+                                                      zoom: 15.0)
+                mapView.animate(to: camera) // Animate to the location
+                hasCenteredOnUser = true // Mark as centered
+            }
         }
 
         return mapView
     }
 
     func updateUIView(_ uiView: GMSMapView, context: Context) {
-        // Update camera position if the current location changes
-        if let currentLocation = locationManager.currentLocation {
-            let camera = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude,
-                                                  longitude: currentLocation.coordinate.longitude,
-                                                  zoom: 15.0)
-            uiView.animate(to: camera)
-        }
-
         // If a place is selected, move the camera to the selected place
         if let place = selectedPlace {
             let camera = GMSCameraPosition.camera(withLatitude: place.coordinate.latitude,
