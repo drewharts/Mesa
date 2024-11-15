87bcef324441c08ddbd13d5b86fb0f22b2bb6ae2
drewhartsfield <drewharts8@gmail.com>
2024-11-14 19:00:50 -0700

    first login worked

---
+++ loc/Services/YourAppCheckProviderFactory.swift
@@ -0,0 +1,11 @@
+import Firebase
+
+class YourAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app)
+        } else {
+            return DeviceCheckProvider(app: app)
+        }
+    }
+}
