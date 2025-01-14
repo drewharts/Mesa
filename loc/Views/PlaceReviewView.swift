b41676a43bb190b18757268232a743e826bc0a41
drewhartsfield <drewharts8@gmail.com>
2025-01-13 21:18:04 -0700

    continued styling improvements

--- loc/Views/PlaceReviewView.swift
+++ loc/Views/PlaceReviewView.swift
@@ -39,22 +39,19 @@ struct PlaceReviewView: View {
                         .font(.largeTitle)
                         .fontWeight(.bold)
                         .foregroundColor(.black)
-                    Text("Your Review")
-                        .font(.footnote)
-                        .foregroundStyle(.black)
                     
                     Divider()
                         .padding(.top, 15)
                         .padding(.bottom, 15)
-                        .padding(.horizontal, -20)
+                        .padding(.horizontal, -10)
 
 
                     VStack(spacing: 8) {
-                        SliderRow(title: "FOOD", value: $foodRating)
+                        SliderRow(title: "Food", value: $foodRating)
                             .accessibilityIdentifier("foodSlider")
-                        SliderRow(title: "SERVICE", value: $serviceRating)
+                        SliderRow(title: "Service", value: $serviceRating)
                             .accessibilityIdentifier("serviceSlider")
-                        SliderRow(title: "AMBIENCE", value: $ambienceRating)
+                        SliderRow(title: "Ambience", value: $ambienceRating)
                             .accessibilityIdentifier("ambienceSlider")
                     }
                     Divider()
@@ -62,53 +59,31 @@ struct PlaceReviewView: View {
                         .padding(.bottom, 10)
                         .padding(.horizontal, -20)
                     
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("UPVOTE FAVORITE DISHES")
-                            .font(.footnote)
-                            .foregroundStyle(.black)
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack {
-                                ForEach(favoriteDishes, id: \.self) { dish in
-                                    Text(dish)
-                                        .padding(8)
-                                        .background(Capsule().fill(Color.gray.opacity(0.2)))
-                                        .onTapGesture {
-                                            favoriteDishes.removeAll { $0 == dish }
-                                        }
-                                }
-                                Button(action: {
-                                    favoriteDishes.append("New Dish") // Replace with actual input logic
-                                }) {
-                                    Image(systemName: "plus")
-                                        .foregroundColor(.black)
-                                        .padding(8)
-                                        .background(Circle().fill(Color.gray.opacity(0.2)))
-                                }
-                            }
-                        }
-                    }
+                    UpvoteFavDishesView()
                     Divider()
                         .padding(.top, 15)
                         .padding(.bottom, 15)
-                        .padding(.horizontal, -20)
+                        .padding(.horizontal, -10)
 
 
                     ZStack(alignment: .topLeading) {
                         TextEditor(text: $reviewText)
+                            .font(.subheadline)
                             .frame(height: 100)
                             .scrollContentBackground(.hidden)
                             .background(.gray.opacity(0.3))
                             .cornerRadius(8)
-                            .foregroundStyle(.white)
+                            .foregroundStyle(.black)
                             .onChange(of: reviewText) {
                                 isReviewTextEmpty = reviewText.isEmpty
                             }
 
                         if isReviewTextEmpty {
-                            Text("Add a review")
+                            Text("Add a review...")
+                                .font(.subheadline)
                                 .foregroundColor(.black)
                                 .font(.footnote)
-                                .padding(.horizontal, 5)
+                                .padding(.horizontal, 10)
                                 .padding(.vertical, 8)
                                 .allowsHitTesting(false)
                         }
@@ -117,20 +92,37 @@ struct PlaceReviewView: View {
                     Divider()
                         .padding(.top, 15)
                         .padding(.bottom, 15)
-                        .padding(.horizontal, -20)
+                        .padding(.horizontal, -10)
+                    
+                    //upload photos
+                    HStack(alignment: .center) {
+                        Image(systemName: "plus")
+                            .foregroundColor(.black)
+                            .padding(8)
+                            .background(Circle().fill(Color.gray.opacity(0.2)))
+                        Text("Upload photos")
+                            .foregroundColor(.black)
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Divider()
+                        .padding(.top, 15)
+                        .padding(.bottom, 15)
+                        .padding(.horizontal, -10)
 
 
                     Button(action: { isPresented = false }) {
-                        Text("Post Review")
+                        Text("POST REVIEW")
                             .bold()
                             .frame(maxWidth: .infinity)
                             .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
+                            .background(Color.gray.opacity(0.2))
+                            .foregroundColor(.black)
+                            .cornerRadius(20)
                     }
                 }
-                .padding(.horizontal, 50)
+                .padding(.horizontal, 40)
             }
             .background(Color(.white))
         }
@@ -139,7 +131,37 @@ struct PlaceReviewView: View {
     }
 }
 
+struct UpvoteFavDishesView: View {
+    @State private var favoriteDishes: [String] = []
 
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Upvote favorite dishes")
+                .font(.footnote)
+                .foregroundStyle(.black)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(favoriteDishes, id: \.self) { dish in
+                        Text(dish)
+                            .padding(8)
+                            .background(Capsule().fill(Color.gray.opacity(0.2)))
+                            .onTapGesture {
+                                favoriteDishes.removeAll { $0 == dish }
+                            }
+                    }
+                    Button(action: {
+                        favoriteDishes.append("New Dish") // Replace with actual input logic
+                    }) {
+                        Image(systemName: "plus")
+                            .foregroundColor(.black)
+                            .padding(8)
+                            .background(Circle().fill(Color.gray.opacity(0.2)))
+                    }
+                }
+            }
+        }
+    }
+}
 struct SliderRow: View {
     let title: String
     @Binding var value: Double
@@ -170,16 +192,22 @@ struct SliderRow: View {
                             })
                         )
 
-                    // Thumb
-                    Circle()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(.black)
-                        .offset(x: geometry.size.width * CGFloat(value / 10.0) - 10) // -10 to center the thumb
-                        .gesture(DragGesture(minimumDistance: 0)
-                            .onChanged({ value in
-                                self.value = min(10.0, max(0.0, Double(value.location.x / geometry.size.width * 10.0)))
-                            })
-                        )
+                    // Thumb with Number
+                    ZStack {
+                        Circle()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.black)
+                        
+                        Text(String(format: "%.00f", value)) // Display the value
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
+                    .offset(x: geometry.size.width * CGFloat(value / 10.0) - 10) // -10 to center the thumb
+                    .gesture(DragGesture(minimumDistance: 0)
+                        .onChanged({ value in
+                            self.value = min(10.0, max(0.0, Double(value.location.x / geometry.size.width * 10.0)))
+                        })
+                    )
                 } // End of ZStack
                 .frame(height: 10)
             }
